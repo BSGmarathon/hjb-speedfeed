e630e2ad37fce00c84726d8cc1d024d14cf2e457
jay <hijumpboots@gmail.com>
2024-01-07 04:12:35 -0600

    Edits to publishFeedGen

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -21,7 +21,7 @@ const run = async () => {
 
   // A display name for your feed
   // Ex: What's Hot
-  const displayName = 'HiJumpBoots SpeedFeed'
+  const displayName = '⏱️SpeedFeed'
 
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
